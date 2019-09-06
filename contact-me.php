19d909e1402acb7079078d0ac7183eabec8046a0
sodiq-web <ayobamisodiq68@gmail.com>
2019-09-06 16:11:15 +0100

    re-adjusted the form method

--- contact-me.php
+++ contact-me.php
@@ -1,17 +1,17 @@
 <?php
 
-if (isset($_GET['submit'])) {
-    $name = $_GET['name'];
-    $mailFrom = $_GET['email'];
-    $messageSubject = $_GET['title'];
-    $message = $_GET['message'];
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $messageSubject = $_POST['title'];
+    $message = $_POST['message'];
 
     $mailTo = "ayobamisodiq68@hotmail.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
     mail($mailTo, $messageSubject, $txt, $headers);
-    echo "Mail Sent. Thank you " . $name . ", we will get back to you shortly.";
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
    // header("Location: sodiq.html?mailsend");
 
 }
