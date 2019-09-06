e3efd54b5c1a547882d508e88b17941dfd4ad0bf
sodiq-web <ayobamisodiq68@gmail.com>
2019-09-06 16:04:33 +0100

    adjusted the form method

--- contact-me.php
+++ contact-me.php
@@ -1,10 +1,10 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $messageSubject = $_POST['title'];
-    $message = $_POST['message'];
+if (isset($_GET['submit'])) {
+    $name = $_GET['name'];
+    $mailFrom = $_GET['email'];
+    $messageSubject = $_GET['title'];
+    $message = $_GET['message'];
 
     $mailTo = "ayobamisodiq68@hotmail.com";
     $headers = "From: ".$mailFrom;
